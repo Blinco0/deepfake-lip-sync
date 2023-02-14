2b74763a07394608dc8dcf9f07cb0cc7f755317f
Khoa Nguyen <dangkhoa81122@gmail.com>
2023-02-14 11:02:41 -0500

    New model
    Former-commit-id: fb9d62a74764e4989afc6864fd6c433912b15a8a

--- process_data_script.py
+++ process_data_script.py
@@ -1,4 +1,7 @@
 from utils.get_face_from_video import extract_data
 
 if __name__ == "__main__":
-    extract_data("/media/khoa/Stuff/part_22", "/home/khoa/Documents/Projects/QMIND/deepfake-lip-sync/dataset")
+    extract_data("/media/khoa/Stuff/part_00", "/media/khoa/Stuff/dataset")
+    extract_data("/media/khoa/Stuff/part_22", "/media/khoa/Stuff/dataset")
+    extract_data("/media/khoa/Stuff/part_33", "/media/khoa/Stuff/dataset")
+    extract_data("/media/khoa/Stuff/part_34", "/media/khoa/Stuff/dataset")
